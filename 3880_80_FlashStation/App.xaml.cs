d1e1d0cef90106a6fea0d4ce6f5405b1021d314b
KH8 <reczek.krzysztof@gmail.com>
2014-03-24 08:28:45 +0100

    Project initialization

---
+++ 3880_80_FlashStation/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _3880_80_FlashStation
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
