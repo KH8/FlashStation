b293f094e253c27580c7e9a01a3bcf66d9b999bc
KH8 <reczek.krzysztof@gmail.com>
2014-03-24 13:38:51 +0100

    PlcComunicator connection methods

--- 3880_80_FlashStation/PLC/PlcCommunicator.cs
+++ 3880_80_FlashStation/PLC/PlcCommunicator.cs
@@ -1,10 +1,13 @@
-﻿using _3880_80_FlashStation.Configuration;
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+using System.Windows;
+using _3880_80_FlashStation.Configuration;
 
 namespace _3880_80_FlashStation.PLC
 {
     class PlcCommunicator
     {
-        #region Properties
+        #region Variables
         
         //Private
         //Status
@@ -17,6 +20,15 @@ namespace _3880_80_FlashStation.PLC
         //Data buffers
         private byte[] _readBufferBytes;
         private byte[] _writeBufferBytes;
+
+        // NoDave variables
+        private libnodave.daveOSserialType _daveOSserialType;
+        private libnodave.daveInterface _daveInterface;
+        private libnodave.daveConnection _daveConnection;
+
+        #endregion
+
+        #region Properties
 
         //Public
         //Status
@@ -80,10 +92,58 @@ namespace _3880_80_FlashStation.PLC
             _connectionStatus = 1;
         }
 
+        public void OpenConnection()
+        {
+            // Check if configuration is done
+            if (_configurationStatus != 1)
+            {
+                throw new PlcException("Plc communication is not configured.");
+            }
+            // Open connection only if was closed
+            if (_connectionStatus == -1)
+            {
+                _daveOSserialType.rfd = libnodave.openSocket(_plcConfiguration.PlcPortNumber, _plcConfiguration.PlcIpAddress);
+                _daveOSserialType.wfd = _daveOSserialType.rfd;
+                if (_daveOSserialType.rfd > 0)
+                {
+                    _daveInterface = new libnodave.daveInterface(_daveOSserialType, "IF1", 0, libnodave.daveProtoISOTCP, libnodave.daveSpeed187k);
+                    _daveInterface.setTimeout(10000); // orginal was:1000000
+                    _daveConnection = new libnodave.daveConnection(_daveInterface, 0, _plcConfiguration.PlcRackNumber, _plcConfiguration.PlcSlotNumber);
 
+                    if (_daveConnection.connectPLC() == 0)
+                        _connectionStatus = 0;
+                    else
+                        _connectionStatus = -1;
+                }
+                else
+                {
+                    throw new PlcException("Can not open connection to plc.");
+                }
+            }
+        }
+        public void CloseConnection()
+        {
+            // Close connection only if was open
+            if (_daveConnection != null)
+            {
+                _daveConnection.disconnectPLC();
+                libnodave.closeSocket(_daveOSserialType.rfd);
+            }
+            _connectionStatus = -1;
+        }
 
         #endregion
 
+        #region Auxiliaries
+
+        public class PlcException : ApplicationException
+        {
+            public PlcException(string info) : base(info)
+            {
+            }
+        }
+
+        #endregion
 
     }
 }
