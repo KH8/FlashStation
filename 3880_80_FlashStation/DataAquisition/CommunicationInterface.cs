b963c3e0782573e5f5b4db90bdf5d84380962739
KH8 <reczek.krzysztof@gmail.com>
2014-03-25 13:54:30 +0100

    Communication interface

--- 3880_80_FlashStation/DataAquisition/CommunicationInterface.cs
+++ 3880_80_FlashStation/DataAquisition/CommunicationInterface.cs
@@ -1,13 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _3880_80_FlashStation.DataAquisition
+﻿namespace _3880_80_FlashStation.DataAquisition
 {
     abstract class CommunicationInterface
     {
+        public struct ReadData
+        {
+            
+        }
 
+        public struct WriteData
+        {
+
+        }
+    }
+
+    abstract class CommunicationInterfaceComponent
+    {
+        private int _id;
+        private int _pos;
+        private string _type;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public int Pos
+        {
+            get { return _pos; }
+            set { _pos = value; }
+        }
+
+        public string Type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+
+        public CommunicationInterfaceComponent()
+        {
+            _id = 0;
+            _pos = 0;
+            _type = "Undefined";
+        }
     }
 }
