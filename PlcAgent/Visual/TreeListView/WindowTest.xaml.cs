b3b6726c09c1dc2468eb0d8ca2c2676307ab6793
KH8 <reczek.krzysztof@gmail.com>
2014-09-04 22:43:56 +0200

    TreeListView example integrated ...

--- PlcAgent/Visual/TreeListView/WindowTest.xaml.cs
+++ PlcAgent/Visual/TreeListView/WindowTest.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace _PlcAgent.Visual.TreeListView
+﻿using System;
+using _PlcAgent.DataAquisition;
+
+namespace _PlcAgent.Visual.TreeListView
 {
     /// <summary>
     /// Interaction logic for WindowTest.xaml
@@ -8,6 +11,42 @@
         public WindowTest()
         {
             InitializeComponent();
+            TestTreeListView.Items.Add(new DisplayDataBuilder.DisplayData
+            {
+                Component = new CiReal("sdas", 0, CommunicationInterfaceComponent.VariableType.Real, 0.9f),
+                Address = "0",
+                Name = "testVal",
+                Type = "asd",
+                Value = "000"
+            });
+            var testHeader = new TreeListViewItem
+            {
+                Header = new DisplayDataBuilder.DisplayData
+                {
+                    Component = new CiReal("sdas", 0, CommunicationInterfaceComponent.VariableType.Real, 0.9f),
+                    Address = "",
+                    Name = "testasdasdasdasdasdVal",
+                    Type = "",
+                    Value = ""
+                } 
+            };
+            testHeader.Items.Add(new DisplayDataBuilder.DisplayData
+            {
+                Component = new CiReal("sdas", 0, CommunicationInterfaceComponent.VariableType.Real, 0.9f),
+                Address = "0",
+                Name = "testVal",
+                Type = "asd",
+                Value = "000"
+            });
+            TestTreeListView.Items.Add(testHeader);
+            TestTreeListView.Items.Add(new DisplayDataBuilder.DisplayData
+            {
+                Component = new CiReal("sdas", 0, CommunicationInterfaceComponent.VariableType.Real, 0.9f),
+                Address = "0",
+                Name = "testVal",
+                Type = "asd",
+                Value = "000"
+            });
         }
     }
 }
