51fafe182c9df454a9254a7c85664af0a695bebd
KH8 <reczek.krzysztof@gmail.com>
2014-10-14 13:41:56 +0200

    DbConnection init

---
+++ PlcAgent/DB/DBConnection.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using _PlcAgent.MainRegistry;
+
+namespace _PlcAgent.DB
+{
+    class DbConnection : ExecutiveModule
+    {
+        #region Properties
+
+        public override string Description
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        #endregion
+
+
+        #region Constructors
+
+        public DbConnection(uint id, string name) : base(id, name)
+        {
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        public override void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Deinitialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void AssignmentFileUpdate()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void CreateInterfaceAssignment()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+    }
+}
